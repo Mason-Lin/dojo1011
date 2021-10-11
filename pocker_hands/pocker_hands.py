f7a3accf9ce6be0d094cad3f96bd469cc98812d9
Mason Lin <pizza0117@gmail.com>
2021-10-11 20:51:23 +0800

    implement parse number (cont.)

--- pocker_hands/pocker_hands.py
+++ pocker_hands/pocker_hands.py
@@ -1,6 +1,22 @@
 class PokerGame:
     def __init__(self, player1, cards1, player2, cards2):
-        pass
+        self.cards1 = self.parse_card_number(cards1)
+        self.player1 = player1
+        self.cards2 =  self.parse_card_number(cards2)
+        self.player2 = player2
+
+    def parse_card_number(self, cards):
+        splited = cards.split(" ")
+        first_char =[i[0] for i in splited]
+        mapping = {
+            "J": 11,
+            "Q": 12,
+            "K": 13,
+            "A": 14
+        }
+        numbers = [mapping[i] for i in first_char]
+        return numbers
 
     def result(self):
-        raise NotImplemented
+        for card in self.cards1:
+
