ca1ab7dd0a94e4e0ffdad3feb32f4f01945fa1b1
ykhorizon-laptop-ubuntu <ykhorizon.light@gmail.com>
2021-10-11 20:14:50 +0800

    add buzz case

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,3 +1,7 @@
 def result(num):
     if num % 3 == 0:
         return "fizz"
+    elif num % 5 == 0:
+        return 'buzz'
+    else:
+        return str(num)
--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -5,3 +5,11 @@ from fizzbuzz import result
 
 def test_should_return_fizz():
     assert result(3) == "fizz"
+    assert result(6) == "fizz"
+
+def test_should_return_number():
+    assert result(1) == "1"
+    assert result(2) == "2"
+
+def test_should_return_buzz():
+    assert result(5) == "buzz"
