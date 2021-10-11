adc56e304987cab6a3013b6bc43bf52b25611712
Mason Lin <pizza0117@gmail.com>
2021-10-11 20:07:49 +0800

    test: :test_tube: first test

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,2 +1,2 @@
 def result(num):
-    raise NotImplemented
+    
--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -1,3 +1,7 @@
 """
     http://codingdojo.org/kata/FizzBuzz/
 """
+from fizzbuzz import result
+
+def test_should_return_fizz():
+    assert result(3) == "fizz"
