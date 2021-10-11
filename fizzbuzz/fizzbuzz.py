c52b4c906c5713e99231519a80cca237f7ef3834
Mason Lin <pizza0117@gmail.com>
2021-10-11 20:19:40 +0800

    refactor: :recycle: early return

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,7 +1,7 @@
 def result(num):
-    if num % 3 == 0:
-        if num % 5 == 0:
-            return "fizzbuzz"
+    if num % 15 == 0:
+        return "fizzbuzz"
+    elif num % 3 == 0:
         return "fizz"
     elif num % 5 == 0:
         return "buzz"
