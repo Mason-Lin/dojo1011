3bdd62f3ae3d2fb6357589c726ff0813c1ce2b66
Mason Lin <pizza0117@gmail.com>
2021-10-11 20:18:26 +0800

    fix: :white_check_mark: fizz buzz pass

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,7 +1,9 @@
 def result(num):
     if num % 3 == 0:
+        if num % 5 == 0:
+            return "fizzbuzz"
         return "fizz"
     elif num % 5 == 0:
-        return 'buzz'
+        return "buzz"
     else:
         return str(num)
